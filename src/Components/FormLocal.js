5c0816a8accd1752cc476df6adfde2c72fd8f700
DioguinhoSantos <diiogosantos@live.com>
2023-02-11 11:29:06 -0300

    sab1102

--- src/Components/FormLocal.js
+++ src/Components/FormLocal.js
@@ -3,19 +3,32 @@ import { Form } from "./style";
 
 export default function FormLocal() {
     const [nome, setNome] = useState("");
-    const [tarefa, setTarefa] = useState("");
+    // const [tarefa, setTarefa] = useState("");
     const [listaTarefa, setListaTarefa] = useState([]);
+
+    const recebeNome = (e) => {
+        setNome(e.target.value);
+    }
+
+    const guardaNome = (e) => {
+        localStorage.setItem('nome', nome);
+    }
+
+    const pegaNome = (e) => {
+        localStorage.getItem('nome');
+        alert(localStorage.getItem('nome'));
+    }
 
     return (
         <Form>
             <h3>Prática 1</h3>
             <label htmlFor="nome">
                 nome:
-                <input name="nome" id="nome" />
+                <input name="nome" id="nome" value={nome} onChange={recebeNome} />
             </label>
             <div>
-                <button>Guardar Dados</button>
-                <button>Acessar Dados</button>
+                <button onClick={guardaNome}>Guardar Dados</button>
+                <button onClick={pegaNome}>Acessar Dados</button>
             </div>
             <br />
             <h3>Prática 2</h3>
